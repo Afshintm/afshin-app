7e1f4ac9a1585e3cf61f4689c26b2ddcd530e376
Afshin Teymoori <afshin_tm@yahoo.com>
2021-10-09 22:19:02 +1100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+        <span>Header</span>
+        </div>
+      </header>
+      <main>
+        <div>
+          <span>Main</span>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Products.ts
@@ -0,0 +1,32 @@
+const products = 
+[
+    {
+      productId:1,
+      shopId:1,
+      name:"Product 1",
+      price:56.35,
+      img:""
+    },
+    {
+        productId:2,
+        shopId:1,
+        name:"Product 2",
+        price:6.45,
+        img:""
+      },
+      {
+        productId:3,
+        shopId:1,
+        name:"Product 3",
+        price:5.75,
+        img:""
+      },
+      {
+        productId:4,
+        shopId:1,
+        name:"Product 4",
+        price:22.95,
+        img:""
+      },
+];
+export default products;
